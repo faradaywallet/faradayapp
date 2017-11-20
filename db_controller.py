d405790bc855d30abdad5592487f9b48d7036337
Bren <b_belen@u.pacific.edu>
2017-11-20 02:37:43 -0800

    Added insert and test insert functions into faraday. Added server support for add, index, profile, and registration pages. Connected database to registration and add.

--- db_controller.py
+++ db_controller.py
@@ -2,23 +2,86 @@ __author__ = 'Bren'
 
 import pymysql.cursors
 
-def connect_DB(c_host, c_port, c_user, c_password, c_db, enc_cc_num):
-    try:
-        connection = pymysql.connect(host = c_host, port= c_port, user = c_user, password = c_password, db= c_db, charset='utf8', cursorclass=pymysql.cursors.DictCursor)
-        print('Connected to database')
+class FaradayDB:
 
+    def __init__(self, c_host, c_port, c_user, c_password, c_db):
+        self.connection = ''
+        self.c_host = c_host
+        self.c_port = c_port
+        self.c_user = c_user
+        self.c_password = c_password
+        self.c_db = c_db
+
+    def start(self):
+        try:
+            self.connection = pymysql.connect(host=self.c_host,
+                                              port=self.c_port,
+                                              user=self.c_user,
+                                              password=self.c_password,
+                                              db=self.c_db,
+                                              charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+            print('DBCONTROL/SUCCESS: Connection successful to', self.c_db)
+        except:
+            print('DBCONTROL/FAIL: Unable to connect to database', self.c_db)
+
+    def insert_user(self, values):
         insert_statement = (
-            "INSERT INTO creditcard (creditcardnum)"
-            "VALUE (%s)"
+            "INSERT INTO user (user, email, createDate, salt, auth, symmetricBox)"
+            "VALUE (%s, %s, %s, %s, %s, %s)"
         )
+        self.insert_row(insert_statement, values)
 
-        data = str(enc_cc_num)
-        with connection.cursor() as cursor:
+    def insert_credit(self, values):
+        insert_statement = (
+            "INSERT INTO card (userid, cardid, createDate, salt, auth, symmetricBox)"
+            "VALUE (%s, %s, %s, %s, %s, %s)"
+        )
+        self.insert_row(insert_statement, values)
+
+    def test_insert_user(self, values):
+        insert_statement = (
+            "INSERT INTO test_table (name, email)"
+            "VALUE (%s, %s)"
+        )
+        self.insert_row(insert_statement, values)
+
+    def test_insert_credit(self, values):
+        insert_statement = (
+            "INSERT INTO test_credit (credit, notes)"
+            "VALUE (%s, %s)"
+        )
+        self.insert_row(insert_statement, values)
+
+    def test_select_credit(self):
+        print('DBCONTROL/LOG: Starting db connection')
+        select_statement = (
+            "SELECT * FROM test_credit"
+        )
+        self.start()
+        rows = self.connection.cursor()
+        rows.execute(select_statement)
+        print('DBCONTROL/SUCCESS: Select query successful:', select_statement)
+        for data in rows:
+            print(data)
+        rows.close()
+        self.close()
+
+    def insert_row(self, insert_statement, values):
+        print('DBCONTROL/LOG: Starting db connection')
+        self.start()
+        with self.connection.cursor() as cursor:
             try:
-                cursor.execute(insert_statement, data)
-                connection.commit()
-                print('Insert successful')
+                cursor.execute(insert_statement, values)
+                self.connection.commit()
+                print('DBCONTROL/SUCCESS: Insert successful:', values)
             except:
-                print('Unable to insert into database')
-    finally:
-        connection.close()
+                print('DBCONTROL/FAIL: Unable to insert into database')
+        self.close()
+
+    def close(self):
+        print('DBCONTROL/LOG: Closing db connection')
+        self.connection.close()
+
+if __name__ == '__main__':
+    db = FaradayDB('localhost', 3310, 'root', 'cybr200', 'faraday')
+    db.test_select_credit()
--- faraday.py
+++ faraday.py
@@ -1,29 +1,52 @@
 __author__ = 'Bren'
 
-from flask import Flask, render_template, Response, request, redirect, url_for
-import db_controller as DB
+from flask import Flask, render_template, Response, request, redirect, url_for, session
+from db_controller import FaradayDB
 
 app = Flask('__main__')
+db = FaradayDB('localhost', 3310, 'root', 'cybr200', 'faraday')
 
 @app.route('/')
 def index():
     print('Opening index')
-    # if request.method == 'POST':
-    #     if request.form['submit'] == 'RegistrationRedirect':
-    #         # return redirect(url_for('register'))
-    #         return render_template('register.html')
-
     return render_template('index.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    print('Opening registration page')
+    print('SERVER/LOG: Opening registration page')
     if request.method == 'POST':
-        print('Test')
+        print('SERVER/LOG: Account created by user')
         name = request.form['username']
+        email = request.form['email']
+        pwd = request.form['password']
         print(name)
+        print(email)
+        print(pwd)
+        # TODO: Server side logic to salt and encrypt password
+        db.test_insert_user(values=(name, email))
     return render_template('register.html')
 
+@app.route('/profile', methods=['POST', 'GET'])
+def profile():
+    print('SERVER/LOG: Opening profile page')
+    # TODO: User authentication for accessing user profile
+    # if authenticate():
+    db.test_select_credit()
+    return render_template('profile.html')
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    print('SERVER/LOG: Opening add cards page')
+    if request.method == 'POST':
+        print('SERVER/LOG: Credit card added by user')
+        ccnum = request.form['card']
+        notes = request.form['notes']
+        print(ccnum)
+        print(notes)
+        # TODO: Server side logic to encrypt credit card and notes using user key
+        db.test_insert_credit(values=(ccnum, notes))
+        return redirect(url_for('profile'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run()
